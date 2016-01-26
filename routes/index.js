f07b41e87677d08c0b747abb3656091e736616b6
HoraceChan <chenhouren2008@gmail.com>
2016-01-27 04:12:29 +0800

    完成登录页面

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: '纯洁的导航' });
+//});
 
-router.get('/jade',require('../views/index').init);
+router.get('/',require('../views/index').init);
+
+router.get('/login',require('../views/index').login);
 
 module.exports = router;
--- views/index.js
+++ views/index.js
@@ -4,5 +4,9 @@
 'use strict';
 
 exports.init = function(req, res){
-    res.render('index',{ title: 'Exp' });
+    res.render('index',{ title: '纯洁的导航' });
+};
+
+exports.login = function(req, res){
+    res.render('login',{ title: '导航登录页' });
 };
