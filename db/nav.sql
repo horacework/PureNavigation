e9d1f73ed00195587e284cd174c8cef1fdf0d471
HoraceChan <chenhouren2008@gmail.com>
2016-07-27 15:33:02 +0800

    更新数据库

--- db/nav.sql
+++ db/nav.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 100109
 File Encoding         : 65001
 
-Date: 2016-07-25 21:11:52
+Date: 2016-07-27 15:32:25
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -40,6 +40,8 @@ CREATE TABLE `cookies` (
   `id` varchar(36) NOT NULL,
   `userid` varchar(36) NOT NULL,
   `login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `logout` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `isLogout` int(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `userid` (`userid`),
   CONSTRAINT `cookies_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`)
@@ -48,6 +50,15 @@ CREATE TABLE `cookies` (
 -- ----------------------------
 -- Records of cookies
 -- ----------------------------
+INSERT INTO `cookies` VALUES ('09694d77-be95-466c-97a8-676e89a40e98', 'f65b5135-2336-11e6-8f9b-00ff9099da81', '2016-07-25 21:40:13', null, '0');
+INSERT INTO `cookies` VALUES ('188cc022-ae54-44dc-b19e-0268ce494f93', 'f65b5135-2336-11e6-8f9b-00ff9099da81', '2016-07-25 21:38:07', null, '0');
+INSERT INTO `cookies` VALUES ('3150fbb9-7aab-41b4-a45a-88cefc584442', 'f65b5135-2336-11e6-8f9b-00ff9099da81', '2016-07-27 14:19:18', null, '0');
+INSERT INTO `cookies` VALUES ('36d44ea6-03eb-4ac7-ad62-c36c622621cb', 'f65b5135-2336-11e6-8f9b-00ff9099da81', '2016-07-25 22:21:50', null, '0');
+INSERT INTO `cookies` VALUES ('493bf41f-f3c0-496d-a594-5726a81a3b75', 'f65b5135-2336-11e6-8f9b-00ff9099da81', '2016-07-27 13:00:52', null, '0');
+INSERT INTO `cookies` VALUES ('80467e2f-8800-4b78-b407-da800bdd564a', 'f65b5135-2336-11e6-8f9b-00ff9099da81', '2016-07-27 14:20:42', null, '0');
+INSERT INTO `cookies` VALUES ('89fb5bb8-d153-4cdf-9486-f49263fa8e05', 'f65b5135-2336-11e6-8f9b-00ff9099da81', '2016-07-25 22:21:50', null, '0');
+INSERT INTO `cookies` VALUES ('8db46395-24e4-4bd2-b09d-8cf84ee664ac', 'f65b5135-2336-11e6-8f9b-00ff9099da81', '2016-07-25 22:31:23', null, '0');
+INSERT INTO `cookies` VALUES ('d391540a-93ef-4ff3-8602-613e169bb7f2', 'f65b5135-2336-11e6-8f9b-00ff9099da81', '2016-07-25 22:19:34', null, '0');
 
 -- ----------------------------
 -- Table structure for links
